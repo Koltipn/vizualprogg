51e620ac39a779647e22d1308cffc9c52c583b2e
Alexandr Kolti <ip217s11@csc.sibsutis.ru>
2024-02-19 21:14:08 +0700

    add 2lab

---
+++ homework2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lab_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
